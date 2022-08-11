b9c47c54c868eed65ab34919346ea6f3df37a90f
Jonathan Manchego Sosa <jonathan571999@gmail.com>
2022-08-11 00:48:52 -0500

    creando servidor basico

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      fs.readFile('./index.html', (err, data) => {
+        res.end(data);
+      });
+    }
+    if (req.url === '/assets/styles/app.css') {
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      fs.readFile('./assets/styles/app.css', (err, data) => {
+        res.end(data);
+      } );
+    }
+});
+
+server.listen(process.env.PORT || 3000);
+
